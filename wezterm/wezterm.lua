dad2c3309fb47a10967332c45e06fbe036f98ec5
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-09-20 22:50:38 +0200

    add visual bell

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -49,6 +49,20 @@ config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 -- config.show_close_tab_button_in_tabs = false
 config.show_new_tab_button_in_tab_bar = false
 
+-- ===
+-- Notifications
+-- ===
+
+config.audible_bell = "Disabled"
+config.visual_bell = {
+  fade_in_function = "EaseIn",
+  fade_in_duration_ms = 150,
+  fade_out_function = "EaseOut",
+  fade_out_duration_ms = 150,
+}
+
+config.colors.visual_bell = "#202020"
+
 -- === Misc ===
 
 -- Fix issues with Swedish keyboard
