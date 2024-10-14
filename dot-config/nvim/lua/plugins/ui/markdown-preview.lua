c17183833c0c2f83107cde742d6375cc3e053e81
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-10-14 10:58:58 +0200

    nvim: add markdown preview plugin

---
+++ dot-config/nvim/lua/plugins/ui/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+  -- Render markdown in browser
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+}
