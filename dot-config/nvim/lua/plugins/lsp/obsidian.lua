2e56d0d3f66383f8db7a4619ed525d37ffcc211b
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-10-19 18:30:26 +0200

    activate obsidian.nvim only inside vaults

--- dot-config/nvim/lua/plugins/lsp/obsidian.lua
+++ dot-config/nvim/lua/plugins/lsp/obsidian.lua
@@ -1,23 +1,22 @@
 return {
   -- Support for "intellisense" in Obsidian vaults
+  -- LATER: Consider https://github.com/oflisback/obsidian-bridge.nvim
   {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+      -- refer to `:h file-pattern` for more examples
+      "BufReadPre "
+        .. vim.fn.expand("~")
+        .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/**/*.md",
+      "BufNewFile " .. vim.fn.expand("~") .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/**/*.md",
+    },
     dependencies = {
-      -- Required.
       "nvim-lua/plenary.nvim",
-
-      -- see below for full list of optional dependencies 👇
     },
     opts = {
       workspaces = {
@@ -29,6 +28,7 @@ return {
           name = "Work",
           path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Work",
         },
+        -- LATER: It is also possible to create a custom function here for "dynamic" workspaces
       },
     },
   },
