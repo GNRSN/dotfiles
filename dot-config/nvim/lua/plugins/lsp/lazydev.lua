a99a2b0473ce0d605f60d148a6310ad8d26566f3
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-11-07 18:37:35 +0100

    add wezterm types

--- dot-config/nvim/lua/plugins/lsp/lazydev.lua
+++ dot-config/nvim/lua/plugins/lsp/lazydev.lua
@@ -5,11 +5,15 @@ return {
     ft = "lua", -- only load on lua files
     opts = {
       library = {
+        "lazy.nvim",
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "wezterm-types", mods = { "wezterm" } },
       },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  -- Optional types, loaded as needed
+  { "Bilal2453/luvit-meta", lazy = true },
+  { "justinsgithub/wezterm-types", lazy = true },
 }
