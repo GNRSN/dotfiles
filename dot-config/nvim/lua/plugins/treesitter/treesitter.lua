f42652d8f4e7e6001afdc51028da7a788a5ce479
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-11-07 22:53:23 +0100

    refactor treesitter config into opts object

--- dot-config/nvim/lua/plugins/treesitter/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -2,71 +2,66 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
     dependencies = {
-      "windwp/nvim-ts-autotag",
     },
-    config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
-
+    build = ":TSUpdate",
+    opts = { -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = {
+        enable = true,
+      },
+      autotag = {
+        enable = false,
+      },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "gitignore",
+        "go",
+        "graphql",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap", -- Lua patterns
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "prisma",
+        "query", -- This is the Treesitter query lang
+        "regex",
+        "rust",
+        "scss",
+        "svelte",
+        "typescript",
+        "tsx",
+        "toml",
+        "yaml",
+        "vim",
+        "vimdoc",
+        "zig",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    },
+    config = function(_, opts)
       -- configure treesitter
-      ---@diagnostic disable-next-line: missing-fields (Type requires all config properties)
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = {
-          enable = true,
-        },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "bash",
-          "css",
-          "dockerfile",
-          "gitignore",
-          "go",
-          "graphql",
-          "html",
-          "javascript",
-          "jsdoc",
-          "json",
-          "jsonc",
-          "lua",
-          "luadoc",
-          "luap", -- Lua patterns
-          "markdown",
-          "markdown_inline",
-          "nix",
-          "prisma",
-          "query", -- This is the Treesitter query lang
-          "regex",
-          "rust",
-          "scss",
-          "svelte",
-          "typescript",
-          "tsx",
-          "toml",
-          "yaml",
-          "vim",
-          "vimdoc",
-          "zig",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
+      require("nvim-treesitter.configs").setup(opts)
 
       -- No mdx treesitter grammar available
       ---@see https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/
