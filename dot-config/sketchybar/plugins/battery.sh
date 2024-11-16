4ceb41c64624823b08d506f83dddf40529a302e0
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-11-16 23:01:36 +0100

    add low battery indicator

--- dot-config/sketchybar/plugins/battery.sh
+++ dot-config/sketchybar/plugins/battery.sh
@@ -1,28 +1,43 @@
 #!/bin/sh
 
-PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+source "$CONFIG_DIR/colors.sh"
+
+# Override battery level for testing (comment out for normal operation)
+# BATTERY_OVERRIDE=50
+
+# Get battery percentage, either from override or system
+if [ -n "$BATTERY_OVERRIDE" ]; then
+  PERCENTAGE=$BATTERY_OVERRIDE
+else
+  PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+fi
+
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
 if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
-  ;;
-  [6-8][0-9]) ICON=""
-  ;;
-  [3-5][0-9]) ICON=""
-  ;;
-  [1-2][0-9]) ICON=""
-  ;;
-  *) ICON=""
-esac
+# Default color (white)
+COLOR="0xffffffff"
 
-if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+# Set icon based on charging status
+if [ -n "$CHARGING" ]; then
+  ICON="􀢋" # Charging icon
+else
+  ICON="􀺶" # Battery icon
+
+  # Set color based on battery level
+  if [ "$PERCENTAGE" -le 10 ]; then
+    COLOR="0xffff0000" # Red
+  elif [ "$PERCENTAGE" -le 30 ]; then
+    COLOR="0xffffd700" # Yellow
+  fi
 fi
 
-# The item invoking this script (name $NAME) will get its icon and label
-# updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+# Only show icon if battery is <= 30%
+if [ "$PERCENTAGE" -le 30 ]; then
+  sketchybar --set "$NAME" icon="$ICON" icon.color="$COLOR" label=""
+else
+  sketchybar --set "$NAME" icon="" label=""
+fi
