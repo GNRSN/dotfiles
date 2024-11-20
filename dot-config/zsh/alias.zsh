5fb4dbd908578c73a8b9f1a03f798ad7e425897b
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-11-20 17:06:27 +0100

    add alias for apple log

--- dot-config/zsh/alias.zsh
+++ dot-config/zsh/alias.zsh
@@ -12,3 +12,6 @@ alias llama="ollama run llama3.1:latest"
 # gcal with monday as first columns
 alias cal='gcal --starting-day=1 "$@"' 
 alias timestamp='$ZDOTDIR/bin/ms-timestamp-to-readable.sh'
+
+# Zsh has a built in log command that may take precedence when simply calling `log`
+alias apple-log='/usr/bin/log'
