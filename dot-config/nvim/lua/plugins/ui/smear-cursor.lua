d948798eb1a957a8ed64f5067b0afacd2787ff31
GNRSN <9020004+GNRSN@users.noreply.github.com>
2025-01-13 19:03:50 +0100

    add smear-cursor.nvim

---
+++ dot-config/nvim/lua/plugins/ui/smear-cursor.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+
+    opts = {
+      -- Smear cursor when switching buffers or windows.
+      smear_between_buffers = true,
+
+      -- Smear cursor when moving within line or to neighbor lines.
+      smear_between_neighbor_lines = true,
+
+      -- Draw the smear in buffer space instead of screen space when scrolling
+      scroll_buffer_space = true,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      legacy_computing_symbols_support = true,
+
+      -- DOC: Faster smear
+      -- stiffness = 0.8,
+      -- trailing_stiffness = 0.5,
+      -- distance_stop_animating = 0.5,
+      -- hide_target_hack = false,
+
+      -- Since we have Noice, the bottom cmd line is hidden
+      smear_to_cmd = false,
+
+      -- Color
+      cursor_color = require("colorscheme.palette").pink,
+    },
+  },
+}
