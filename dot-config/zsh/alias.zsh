5ad027c7d420c925131f4809b38a72443a58e484
GNRSN <9020004+GNRSN@users.noreply.github.com>
2025-01-23 09:25:47 +0100

    add git-commits alias

--- dot-config/zsh/alias.zsh
+++ dot-config/zsh/alias.zsh
@@ -15,3 +15,25 @@ alias timestamp='$ZDOTDIR/bin/ms-timestamp-to-readable.sh'
 
 # Zsh has a built in log command that may take precedence when simply calling `log`
 alias apple-log='/usr/bin/log'
+
+# https://gist.github.com/akatrevorjay/9fc061e8371529c4007689a696d33c62
+git-commits() {
+	local g=(
+		git log
+		--graph
+		--format='%C(auto)%h%d %s %C(white)%C(bold)%cr'
+		--color=always
+		"$@"
+	)
+
+	local fzf=(
+		fzf
+		--ansi
+		--reverse
+		--tiebreak=index
+		--no-sort
+		--bind=ctrl-s:toggle-sort
+		--preview 'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1; }; f {}'
+	)
+	$g | $fzf
+}
