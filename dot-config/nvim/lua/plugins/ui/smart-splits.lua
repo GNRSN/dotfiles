cb4425e99d639873790f1e04448c86f1a80b8a4c
GNRSN <9020004+GNRSN@users.noreply.github.com>
2025-03-10 09:34:56 +0100

    change smart-splits keybinds to jump in insert and visual modes

--- dot-config/nvim/lua/plugins/ui/smart-splits.lua
+++ dot-config/nvim/lua/plugins/ui/smart-splits.lua
@@ -4,44 +4,89 @@ return {
     "mrjones2014/smart-splits.nvim",
     -- DOC: Lazy loading may cause issues
     lazy = false,
-    config = function()
-      require("smart-splits").setup({
-        -- I wasn't really a fan of wrapping
-        at_edge = "stop",
-
-        ignored_filetypes = {
-          "neo-tree",
-          "neotest",
-          "neotest-summary",
-          "neotest-output-panel",
-          "Trouble",
-          "noice",
-          "undotree",
-          "spectre_panel",
-          "octo",
-          "DiffviewFiles",
-        },
-      })
-
-      -- recommended mappings
+    keys = {
+      -- moving between splits
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+        mode = { "n", "i", "x" },
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+        mode = { "n", "i", "x" },
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+        mode = { "n", "i", "x" },
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        mode = { "n", "i", "x" },
+      },
       -- resizing splits
       -- these keymaps will also accept a range,
       -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-      vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-      vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-      vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-      vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
-      -- moving between splits
-      vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-      vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-      vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-      vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-      -- vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+      {
+        "<A-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+        mode = { "n", "i", "x" },
+      },
+      {
+        "<A-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+        mode = { "n", "i", "x" },
+      },
+      {
+        "<A-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+        mode = { "n", "i", "x" },
+      },
+      {
+        "<A-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+        mode = { "n", "i", "x" },
+      },
       -- swapping buffers between windows
       -- vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
       -- vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
       -- vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
       -- vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
-    end,
+    },
+    opts = {
+      -- I wasn't really a fan of wrapping
+      at_edge = "stop",
+
+      ignored_filetypes = {
+        "neo-tree",
+        "neotest",
+        "neotest-summary",
+        "neotest-output-panel",
+        "Trouble",
+        "noice",
+        "undotree",
+        "spectre_panel",
+        "octo",
+        "DiffviewFiles",
+      },
+    },
   },
 }
