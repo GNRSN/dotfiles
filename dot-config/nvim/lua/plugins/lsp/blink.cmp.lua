9909d9993ad496edc7c4f06df7a84ce3566bd356
GNRSN <9020004+GNRSN@users.noreply.github.com>
2025-04-22 17:19:36 +0200

    more experiments with blink.cmp config

--- dot-config/nvim/lua/plugins/lsp/blink.cmp.lua
+++ dot-config/nvim/lua/plugins/lsp/blink.cmp.lua
@@ -1,3 +1,42 @@
+-- check if current line is inside a comment block
+-- DOC: Copied from https://github.com/Saghen/blink.cmp/pull/1354
+local function inside_comment_block()
+  if vim.api.nvim_get_mode().mode ~= "i" then
+    return false
+  end
+  local node_under_cursor = vim.treesitter.get_node()
+  local parser = vim.treesitter.get_parser(nil, nil, { error = false })
+  local query = parser and vim.treesitter.query.get(parser:lang(), "highlights")
+  if not parser or not node_under_cursor or not query then
+    return false
+  end
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  row = row - 1
+  for id, node, _ in query:iter_captures(node_under_cursor, 0, row, row + 1) do
+    if query.captures[id]:find("comment") then
+      local start_row, start_col, end_row, end_col = node:range()
+      if start_row <= row and row <= end_row then
+        if start_row == row and end_row == row then
+          if start_col <= col and col <= end_col then
+            return true
+          end
+        elseif start_row == row then
+          if start_col <= col then
+            return true
+          end
+        elseif end_row == row then
+          if col <= end_col then
+            return true
+          end
+        else
+          return true
+        end
+      end
+    end
+  end
+  return false
+end
+
 local MENU_MAPPER = {
   Buffer = "Buf",
   LSP = "Lsp",
@@ -63,18 +102,22 @@ return {
       keymap = {
         preset = "none",
         ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-        ["<C-space>"] = {
-          function(cmp)
-            if cmp.is_menu_visible() then
-              return cmp.accept()
-            else
-              return cmp.show()
-            end
-          end,
-        },
+        ["<C-space>"] = { "show", "accept" },
+        --   {
+        --   function(cmp)
+        --     if cmp.is_menu_visible() then
+        --       return cmp.accept()
+        --     else
+        --       return cmp.show()
+        --     end
+        --   end,
+        -- },
         ["<C-p>"] = { "select_prev", "fallback" },
         ["<C-n>"] = { "select_next", "fallback" },
         ["<C-q>"] = { "hide", "fallback" },
+        -- Temp until mapping with AI accept
+        ["<tab>"] = { "snippet_forward", "fallback" },
+        ["<s-tab>"] = { "snippet_backward", "fallback" },
       },
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -89,6 +132,9 @@ return {
       },
 
       completion = {
+        keyword = {
+          range = "prefix",
+        },
         accept = {
           -- experimental auto-brackets support
           auto_brackets = {
@@ -103,7 +149,8 @@ return {
         },
 
         menu = {
-          -- border = "single",
+          -- not working
+          auto_show = not inside_comment_block(),
           draw = {
             treesitter = { "lsp" },
             columns = {
@@ -121,24 +168,28 @@ return {
         },
         documentation = {
           window = { border = "single" },
-          auto_show = true,
+          auto_show = false,
           auto_show_delay_ms = 200,
         },
         ghost_text = {
           enabled = true,
         },
         trigger = {
+          -- TODO: Something feels broken here, I don't get lsp suggestions in js objects
+          -- it also doesn't update suggestions as I'm typing
+          -- DOC: set to empty map override default behavior (hiding)
           show_on_blocked_trigger_characters = {},
         },
       },
 
       fuzzy = {
+        -- TODO: remove emmet guessing react components based on capital letter
         sorts = {
           function(a, b)
             if a.client_name == nil or b.client_name == nil then
               return
             end
-            return b.client_name == "emmet_ls"
+            return a.client_name == "emmet_ls"
           end,
           -- DOC: (optionally) always prioritize exact matches
           "exact",
@@ -243,10 +294,8 @@ return {
         end
       end
 
-      -- add ai_accept to <Tab> key
-      -- LATER: I want to learn C-space first completion as it seems most efficient
-      --
-      -- LATER: There are also doc recipes for showing ai ghost text before completion is opened
+      -- LATER: add ai_accept to <Tab> key
+      -- There are also doc recipes for showing ai ghost text before completion is opened
       --
       if not opts.keymap["<Tab>"] then
         -- if opts.keymap.preset == "super-tab" then -- super-tab
