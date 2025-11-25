262c38e674fdfab294b3421a5fbb7bbe3df8f551
GNRSN <9020004+GNRSN@users.noreply.github.com>
2025-11-25 09:30:56 +0100

    nvim: remove superflous termguicolors option set

--- dot-config/nvim/lua/plugins/ui/ccc-color-picker.lua
+++ dot-config/nvim/lua/plugins/ui/ccc-color-picker.lua
@@ -2,9 +2,6 @@ return {
   {
     "uga-rosa/ccc.nvim",
     config = function()
-      -- Enable true color
-      vim.opt.termguicolors = true
-
       local ccc = require("ccc")
 
       ccc.setup({
